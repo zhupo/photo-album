737849dff0c76b3435e940d2aaece90c5c3c0bcd
Zhu Po <1738405457@qq.com>
2020-08-26 14:46:57 +0800

    update frontend main

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
+use frontend\assets\AppAsset;
 
 AppAsset::register($this);
 ?>
