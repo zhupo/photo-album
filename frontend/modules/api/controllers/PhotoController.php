2e3ef4a2d1a1c3803bf4cb9e4c008cc543d13dca
Mao Ye <1738405457@qq.com>
2020-10-08 14:15:28 +0800

    update photo api

--- frontend/modules/api/controllers/PhotoController.php
+++ frontend/modules/api/controllers/PhotoController.php
@@ -95,12 +95,27 @@ class PhotoController extends BaseController
         return [
             [
                 'src' => 'http://a1.qpic.cn/psc?/V50AYnUV3WqjyS2vXqPa0J2GSy2XfUEO/ruAMsa53pVQWN7FLK88i5tG1yOqnqwodaHYWwyQg3yWkEvuZSuy7WfmOWMpwSpTP81vRrCDXDwV3tzBaNTAs3DYL2OzpSiSSjq48u62tUNs!/b&ek=1&kp=1&pt=0&bo=9AEgAQAAAAABF.Q!&tl=3&vuin=1738405457&tm=1600174800&sce=60-4-3&rf=viewer_4',
+                'msrc' => 'http://a1.qpic.cn/psc?/V50AYnUV3WqjyS2vXqPa0J2GSy2XfUEO/ruAMsa53pVQWN7FLK88i5tG1yOqnqwodaHYWwyQg3yWkEvuZSuy7WfmOWMpwSpTP81vRrCDXDwV3tzBaNTAs3DYL2OzpSiSSjq48u62tUNs!/b&ek=1&kp=1&pt=0&bo=9AEgAQAAAAABF.Q!&tl=3&vuin=1738405457&tm=1600174800&sce=60-4-3&rf=viewer_4',
+                'alt' => 'picture1',
+                'title' => 'Image caption 1',
+                'w' => 600,
+                'h' => 400
             ],
             [
-                'src' => 'http://a1.qpic.cn/psc?/V50AYnUV3WqjyS2vXqPa0J2GSy2XfUEO/ruAMsa53pVQWN7FLK88i5tG1yOqnqwodaHYWwyQg3yWkEvuZSuy7WfmOWMpwSpTP81vRrCDXDwV3tzBaNTAs3DYL2OzpSiSSjq48u62tUNs!/b&ek=1&kp=1&pt=0&bo=9AEgAQAAAAABF.Q!&tl=3&vuin=1738405457&tm=1600174800&sce=60-4-3&rf=viewer_4',
+                'src' => 'http://a1.qpic.cn/psc?/V50AYnUV3WqjyS2vXqPa0J2GSy2XfUEO/ruAMsa53pVQWN7FLK88i5iryu2UqRZ4RZQv7Q2NXTKVcIooV34I9PtLymS8.eVbeE3Kbx2SX0Y5jRawUJ8G24ltVHhJHCxBjJOUvqgYbbNY!/m&ek=1&kp=1&pt=0&bo=FQIsAQAAAAABFwo!&t=5&tl=3&vuin=1738405457&tm=1600174800&sce=60-4-3&rf=0-0',
+                'msrc' => 'http://a1.qpic.cn/psc?/V50AYnUV3WqjyS2vXqPa0J2GSy2XfUEO/ruAMsa53pVQWN7FLK88i5iryu2UqRZ4RZQv7Q2NXTKVcIooV34I9PtLymS8.eVbeE3Kbx2SX0Y5jRawUJ8G24ltVHhJHCxBjJOUvqgYbbNY!/m&ek=1&kp=1&pt=0&bo=FQIsAQAAAAABFwo!&t=5&tl=3&vuin=1738405457&tm=1600174800&sce=60-4-3&rf=0-0',
+                'alt' => 'picture2',
+                'title' => 'Image caption 2',
+                'w' => 600,
+                'h' => 400
             ],
             [
-                'src' => 'http://a1.qpic.cn/psc?/V50AYnUV3WqjyS2vXqPa0J2GSy2XfUEO/ruAMsa53pVQWN7FLK88i5tG1yOqnqwodaHYWwyQg3yWkEvuZSuy7WfmOWMpwSpTP81vRrCDXDwV3tzBaNTAs3DYL2OzpSiSSjq48u62tUNs!/b&ek=1&kp=1&pt=0&bo=9AEgAQAAAAABF.Q!&tl=3&vuin=1738405457&tm=1600174800&sce=60-4-3&rf=viewer_4',
+                'src' => 'http://a1.qpic.cn/psc?/V50AYnUV3WqjyS2vXqPa0J2GSy2XfUEO/ruAMsa53pVQWN7FLK88i5s0v5gMdxzWwJnpWzDAgpjkF1713LJOFp12bc5f*nl1FY.h7LpPEuGYV7jJum5YTY8BxktsMoSsjCI2Ju1sEM34!/m&ek=1&kp=1&pt=0&bo=0gEsAQAAAAABF84!&t=5&tl=3&vuin=1738405457&tm=1600174800&sce=60-4-3&rf=0-0',
+                'msrc' => 'http://a1.qpic.cn/psc?/V50AYnUV3WqjyS2vXqPa0J2GSy2XfUEO/ruAMsa53pVQWN7FLK88i5s0v5gMdxzWwJnpWzDAgpjkF1713LJOFp12bc5f*nl1FY.h7LpPEuGYV7jJum5YTY8BxktsMoSsjCI2Ju1sEM34!/m&ek=1&kp=1&pt=0&bo=0gEsAQAAAAABF84!&t=5&tl=3&vuin=1738405457&tm=1600174800&sce=60-4-3&rf=0-0',
+                'alt' => 'picture3',
+                'title' => 'Image caption 3',
+                'w' => 600,
+                'h' => 400
             ],
         ];
     }
